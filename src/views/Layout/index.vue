18a98bb34cce4b188335d80da9a0cca575fa26f3
yiliya <2240273312@qq.com>
2024-10-25 02:01:35 +0800

    解决路由器缓存

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -13,6 +13,8 @@ onMounted(() => categoryStore.getCategory())
     <LayoutFixed />
     <LayoutNav />
     <LayoutHeader />
+    <!-- 添加key破坏复用机制，强制销毁重建 -->
+    <!-- <RouterView :key="$route.fullPath" /> -->
     <RouterView />
     <LayoutFooter />
 </template>
