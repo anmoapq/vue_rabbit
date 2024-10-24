f8888f731d9ed6bdc51f6dad0089d10f0f2997fd
yiliya <2240273312@qq.com>
2024-10-24 21:38:31 +0800

    插槽

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,8 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import Homepanel from './components/Homepanel.vue'
+
 </script>
 
 <template>
@@ -14,4 +16,11 @@ import homeProduct from './components/HomeProduct.vue'
     <HomeNew />
     <HomeHot />
     <homeProduct />
+    <!-- 测试面板组件 -->
+    <Homepanel title="新鲜好物" sub-title="新鲜好物好多商品">
+        <div>我是信息好物插槽</div>
+    </Homepanel>
+    <Homepanel title="人气推荐" sub-title="人气推荐好多商品">
+        <div>我是信息好物插槽</div>
+    </Homepanel>
 </template>
