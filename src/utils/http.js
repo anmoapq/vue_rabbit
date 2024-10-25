889681ac0864c19284c65daa2e6eb7adb4c28b9c
yiliya <2240273312@qq.com>
2024-10-25 23:16:35 +0800

    请求拦截器

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
+
+
 // 创建axios实例
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -9,6 +12,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    //从pinia获取token
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    //对token进行拼接
     return config
 }, e => Promise.reject(e))
 
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,7 +1,6 @@
 <script setup>
-
-
-
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore();
 </script>
 
 <template>
@@ -9,8 +8,9 @@
         <div class="container">
             <ul>
                 <!-- 多模版渲染 -->
-                <template v-if="false">
-                    <li><a href="javascript:;"><i class=" iconfont icon-user"></i>周杰伦</a></li>
+                <template v-if="userStore.userInfo.token">
+                    <li><a href="javascript:;"><i class=" iconfont icon-user"></i>{{ userStore.userInfo.account }}</a>
+                    </li>
                     <li>
                         <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
                             <template #reference>
