3a57dae6b2c4e5e06ace95f0ae82cb984e4dcc6d
yiliya <2240273312@qq.com>
2024-10-25 19:44:54 +0800

    图片预览组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue';
+import imageView from '@/components/imageView/index.vue'
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -23,7 +24,7 @@ onMounted(() => getGoods())
                     </el-breadcrumb-item>
                     <el-breadcrumb-item :to="{ path: `/category/sub/${goods.categories[0].id}` }">{{
                         goods.categories[0].name
-                    }}
+                        }}
                     </el-breadcrumb-item>
                     <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
                 </el-breadcrumb>
@@ -34,7 +35,7 @@ onMounted(() => getGoods())
                     <div class="goods-info">
                         <div class="media">
                             <!-- 图片预览区 -->
-
+                            <imageView />
                             <!-- 统计数量 -->
                             <ul class="goods-sales">
                                 <li>
