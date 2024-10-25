13e0c6109771842c1596fd3404d1b7faa2ad7c6d
yiliya <2240273312@qq.com>
2024-10-25 15:04:23 +0800

    二级分类商品列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { getCategoryFilterAPI } from '@/apis/category';
+import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category';
 import { onMounted } from 'vue';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
-
+import GoodsItem from '../Home/components/GoodsItem.vue';
 //获取面包屑导航数据
 const categoryData = ref({})
 const route = useRoute()
@@ -12,6 +12,24 @@ const getCategoryData = async () => {
     categoryData.value = res.result
 }
 onMounted(() => getCategoryData())
+
+//获取基础列表数据
+// 获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime'
+})
+
+const getGoodList = async () => {
+    const res = await getSubCategoryAPI(reqData.value)
+    console.log(res)
+    goodList.value = res.result.items
+}
+
+onMounted(() => getGoodList())
 </script>
 
 <template>
@@ -33,10 +51,11 @@ onMounted(() => getCategoryData())
             </el-tabs>
             <div class="body">
                 <!-- 商品列表-->
+                <!-- 商品列表-->
+                <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
             </div>
         </div>
     </div>
-
 </template>
 
 
