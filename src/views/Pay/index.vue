daf2421100aa10636db793201e45e04f71c2adab
yiliya <2240273312@qq.com>
2024-10-26 21:38:20 +0800

    支付结果展示

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -10,6 +10,14 @@ const getPayInfo = async () => {
     payInfo.value = res.data
 }
 onMounted(() => getPayInfo())
+
+//跳转支付
+//携带订单id跳转
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
+
 </script>
 
 
